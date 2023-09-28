137d744e1782a0c8a2c00ee333e1e414c370f5e9
sempreverde1919 <110019528+sempreverde1919@users.noreply.github.com>
2023-09-28 12:05:44 +0000

    Solution of homework

--- currency-calculator/CurrencyCalculator.java
+++ currency-calculator/CurrencyCalculator.java
@@ -6,20 +6,37 @@ public class CurrencyCalculator{
     public static void main(String []args){
         Scanner scanner = new Scanner(System.in);
         int[] currencies = {20000,10000,5000,2000,1000,500,200,100,50,20,10,5};
+    //felhasználói input bekérése
         System.out.print("Adja meg a kért pénzösszeget: ");
         int moneyAmount = scanner.nextInt();
         scanner.nextLine();
-
+    //felhasználói input validációja
+        while(moneyAmount<1 || moneyAmount%5!=0){
+            if(moneyAmount<1){
+                System.out.println("HIBA! Csak 0-nál nagyobb összeg fizethető ki.");
+                System.out.print("Adja meg a kért pénzösszeget: ");
+                moneyAmount = scanner.nextInt();
+                scanner.nextLine();
+            }
+            if(moneyAmount%5!=0){
+                System.out.println("HIBA! A kért címlet nem fizethető ki.");
+                System.out.print("Adja meg a kért pénzösszeget: ");
+                moneyAmount = scanner.nextInt();
+                scanner.nextLine();
+            }
+        }
+    //szükséges címletek kiszámítása és kiírása
         int remainingAmount = moneyAmount;
         int[] pieces = new int[currencies.length];
+        System.out.println("A kért összeg " + moneyAmount + " Ft.");
+        System.out.println("A gép a következő címleteket fizeti ki:");
         for(int i=0; i<currencies.length; i++){
             pieces[i] = remainingAmount / currencies[i];
             remainingAmount = remainingAmount % currencies[i];
+            if(pieces[i]!=0){
+                System.out.println("    " + currencies[i] + " Ft - " + pieces[i] + " db");
+            }
         }
-
-        System.out.println(Arrays.toString(pieces));      
-
-        
     }
 
 }
