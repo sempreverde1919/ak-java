d5df829987be8a0c4443f73a44785efe4d792496
sempreverde1919 <110019528+sempreverde1919@users.noreply.github.com>
2023-09-29 08:52:40 +0000

    Homework of second video (Part 21)

--- basic-game/BasicGame.java
+++ basic-game/BasicGame.java
@@ -7,60 +7,37 @@ public class BasicGame{
       int col = 2;
       Direction direction = Direction.RIGHT;
 
-   //pálya inicializálása
-      for(int i = 0; i<level.length; i++){
-         for(int j = 0; j<level[i].length; j++){
-            if(i==0 || i==level.length-1 || j==0 || j==level[i].length-1){
-               level[i][j] = "X";
+      initLevel(level); //pálya inicializálása
+
+      for(int k=1; k<=100; k++){
+         if(k%10==0){ //irányváltás 10 körönként
+            direction = changeDirection(direction);
+         }
+         
+        int[] coordinates = makeMove(direction, level, row, col); //irányváltás ha falhoz ér
+        row = coordinates[0];
+        col = coordinates[1];   
+      
+        draw(level, playerMarker, row, col);//pálya és játékos kirajzolása
+
+        System.out.println("------------");
+        Thread.sleep(500);
+      }
+ }
+ 
+ //--------------METHODS:
+
+   static void initLevel(String[][] board){
+      for(int i = 0; i<board.length; i++){
+         for(int j = 0; j<board[i].length; j++){
+            if(i==0 || i==board.length-1 || j==0 || j==board[i].length-1){
+               board[i][j] = "X";
             }else{
-               level[i][j] = " ";         
+               board[i][j] = " ";         
             }
          }
       }
-   
-
-   for(int k=1; k<=100; k++){
-      if(k%10==0){ //irányváltás 10 körönként
-         direction = changeDirection(direction);
-      }
-      
-      switch(direction){//irányváltás ha falhoz ér
-         case UP:
-            if(level[row-1][col].equals(" ")){
-               row--;
-            }else{
-               direction = Direction.RIGHT;
-            }
-            break;
-         case DOWN:
-            if(level[row+1][col].equals(" ")){
-               row++;
-            }else{
-               direction = Direction.LEFT;
-            }
-            break;
-         case LEFT:
-            if(level[row][col-1].equals(" ")){
-               col--;
-            }else{
-               direction = Direction.UP;
-            }
-            break;
-         case RIGHT:
-            if(level[row][col+1].equals(" ")){
-               col++;
-            }else{
-               direction = Direction.DOWN;
-            }
-            break;
-      }
-   
-      draw(level, playerMarker, row, col);//pálya és játékos kirajzolása
-
-      System.out.println("------------");
-      Thread.sleep(1000);
    }
- }
 
    static void draw(String[][] board, String mark, int x, int y){
       for(int i = 0; i<board.length; i++){
@@ -78,19 +55,49 @@ public class BasicGame{
    static Direction changeDirection(Direction direction){
       switch(direction){
          case UP:
-            direction = Direction.RIGHT;
-            break;
+            return Direction.RIGHT;
          case DOWN:
-            direction = Direction.LEFT;
-            break;
+            return Direction.LEFT;
          case LEFT:
-            direction = Direction.UP;
-            break;
+            return Direction.UP;
          case RIGHT:
-            direction = Direction.DOWN;
-            break;
+            return Direction.DOWN;
       }
       return direction;
+   }
+
+   static int[] makeMove(Direction direction, String[][] board, int x, int y){
+      switch(direction){
+         case UP:
+            if(board[x-1][y].equals(" ")){
+               x--;
+            }else{
+               direction = Direction.RIGHT;
+            }
+            break;
+         case DOWN:
+            if(board[x+1][y].equals(" ")){
+               x++;
+            }else{
+               direction = Direction.LEFT;
+            }
+            break;
+         case LEFT:
+            if(board[x][y-1].equals(" ")){
+               y--;
+            }else{
+               direction = Direction.UP;
+            }
+            break;
+         case RIGHT:
+            if(board[x][y+1].equals(" ")){
+               y++;
+            }else{
+               direction = Direction.DOWN;
+            }
+            break;
+         }
+         return new int[] {x,y};
    }
 
 }
